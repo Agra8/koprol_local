6855ec6e59011135c20d2df1c82587191c4b9a3d
anggarbagus <anggar.bagus@gmail.com>
2022-01-27 16:22:48 +0700

    [IMP] add company_id and change constrainst

--- eps_branch/models/eps_divisi.py
+++ eps_branch/models/eps_divisi.py
@@ -5,8 +5,24 @@ class Divisi(models.Model):
     _name = "eps.divisi"
     _description = 'Divisi'
 
+    def _get_company(self):
+        res_user = self.env['res.users'].browse(self._uid)
+        return res_user.company_id.id
+
     name = fields.Char(string='Name', required=True)
+    company_id = fields.Many2one('res.company', string='Company', index=True,default=_get_company)
     
-    _sql_constraints = [
-        ('name_unique', 'unique(name)', 'Nama Divisi tidak boleh ada yang sama.')
-    ] 
+    @api.constrains('company_id','name')
+    def _check_name(self):
+        for record in self:
+            division = self.search([('company_id','=',record.company_id.id),('name','=',record.name),('id','!=',record.id)])
+            if division:
+                raise ValidationError('Nama sudah pernah digunakan!')
+
+    def init(self):
+        self._cr.execute("""
+            ALTER TABLE IF EXISTS eps_divisi 
+            DROP CONSTRAINT IF EXISTS eps_divisi_name_unique
+            
+        """
+        ) 
