ad415a41f3c4bce4cbe6045f0737fac24213775f
shendypj18 <shendypj18@gmail.com>
2022-07-30 09:42:54 +0700

    delete api.multi

--- eps_request_form/models/eps_request_form.py
+++ eps_request_form/models/eps_request_form.py
@@ -709,7 +709,7 @@ class RequestFormLine(models.Model):
             }
         }
     
-    @api.multi
+
     def _notify_get_reply_to(self, default=None, records=None, company=None, doc_names=None):
         """ Override to set alias of tickets to their team if any. """
         aliases = self.mapped('teams_id')._notify_get_reply_to(default=default, records=None, company=company, doc_names=None)
