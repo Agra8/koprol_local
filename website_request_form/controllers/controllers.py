42fb970976b530fb1aca39e8b4543da3da6777ab
shendypj18 <shendypj18@gmail.com>
2022-09-05 11:06:51 +0700

    add try except

--- website_request_form/controllers/controllers.py
+++ website_request_form/controllers/controllers.py
@@ -110,7 +110,6 @@ class WebsiteForm(Home):
         if 'request_sistem_ids' in value:
             del value['request_sistem_ids']
         
-        request_id = request.env[model_record.model].sudo().create(value)
         try:
             request_id = request.env[model_record.model].sudo().create(value)
         except Exception as err:
